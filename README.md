c836b49b8b427c9cd77b99059dbbce6644a487aa
watataku8911 <watato.takuya@gmail.com>
2021-09-08 13:04:04 +0900

    first-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# X'mas special site at 2020 | Watataku's
+
+- URL → [http://watataku.php.xdomain.jp/christmas/2020](http://watataku.php.xdomain.jp/christmas/2020).
---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer>
+    <div class="sns">
+      <ShareFacebook color="black" class="footer-link-facebook"/>
+      <div class="module--spacing--very--small"></div>
+      <ShareTwitter color="black" class="footer-link-twitter" />
+    </div>
+    <small>&copy;2020 Watataku.</small>
+  </footer>
+</template>
+
+<script>
+import ShareTwitter from "../components/ShareTwitter";
+
+import ShareFacebook from "../components/ShareFacebook";
+export default {
+  components: {
+    ShareTwitter,
+    ShareFacebook
+  }
+}
+</script>
+
+<style scoped>
+
+.module--spacing--very--small {
+  height: 5px;
+}
+
+footer {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding-top: 1vh;
+  padding-bottom: 1vh;
+  height: 30vh;
+  background-color: #ddd;
+}
+
+.sns {
+  position: absolute;
+  bottom: 3vh;
+  right: 3vh;
+}
+
+small {
+  font-size: 1em;
+  letter-spacing: 2px;
+}
+</style>
